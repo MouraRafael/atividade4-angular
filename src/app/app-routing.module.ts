dab56be02a875e32e6048892def0f44798aebd4a
Rafael Moura <rafael.g.moura@hotmail.com>
2022-11-09 19:39:06 -0300

    feat: registration routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistrationComponent } from './customers/components/registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'clientes/cadastro',
+    component: RegistrationComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
