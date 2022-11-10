ce8291fc2bfb0dbc78c39b577ffe4e19560ee922
Rafael Moura <rafael.g.moura@hotmail.com>
2022-11-10 19:37:26 -0300

    feat: listagem

--- src/app/customers/components/list/list.component.ts
+++ src/app/customers/components/list/list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { ClienteModel } from '../../model/ClienteModel';
+import { CustomerService } from '../../service/customer.service';
 
 @Component({
   selector: 'app-list',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  clientes!:ClienteModel[];
+  displayedColumns:string[] = ['nome','telefone','editar','remover']
+  dataSource!:MatTableDataSource<ClienteModel>;
 
-  constructor() { }
+  constructor(private service:CustomerService, private router:Router) { }
 
   ngOnInit(): void {
+    this.clientes = this.service.list();
+    this.dataSource = new MatTableDataSource(this.clientes);
+
+    console.log(this.clientes)
+  }
+
+  list(){
+    return this.clientes;
   }
 
 }
--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -3,17 +3,23 @@ import { CommonModule } from '@angular/common';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
-import { MatButtonModule } from "@angular/material/button";
 import { CustomerService } from './service/customer.service';
 import { HttpClientModule } from '@angular/common/http';
 import { ListComponent } from './components/list/list.component';
 
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatTableModule } from "@angular/material/table";
+
+
 const materialModules =[
   MatFormFieldModule,
+  MatButtonModule,
   MatInputModule,
-  MatButtonModule
+  MatIconModule,
+  MatTableModule,
 ]
 
 @NgModule({
