d0888b3f36d0efa87e6da7b2391f9b4f371accc5
Rafael Moura <rafael.g.moura@hotmail.com>
2022-11-10 06:57:36 -0300

    feat: formBuilder

--- src/app/customers/components/registration/registration.component.ts
+++ src/app/customers/components/registration/registration.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup, Validators } from "@angular/forms";
+
+import { ClienteModel } from '../../model/ClienteModel';
+import { EnderecoModel } from "../../model/EnderecoModel";
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+  formCadastroCliente!:FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder:FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.formCadastroCliente = this.formBuilder.group({
+      nome:[''],
+      cpf:[''],
+      telefone:[''],
+      endereco:this.formBuilder.group({
+        cep:[''],
+        logradouro:[''],
+        complemento:[''],
+        bairro:[''],
+        localidade:[''],
+        uf:['']
+      }),
+    })
   }
 
+
+
+  cadastrar():void{
+
+    const cliente = this.formCadastroCliente.getRawValue() as ClienteModel;
+    console.log(cliente)
+  }
 }
